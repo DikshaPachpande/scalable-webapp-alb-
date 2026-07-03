068bc77da0ad0b56febb959f087e026108c8eeeb
DikshaPachpande <dikshapachpande@gmail.com>
2026-07-03 18:30:36 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+yum update -y
+yum install python3 git -y
+
+pip3 install flask gunicorn
+
+mkdir /home/ec2-user/app
+cd /home/ec2-user/app
+
+cat > app.py << EOF
+from flask import Flask
+import socket
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"<h1>Server: {socket.gethostname()}</h1>"
+
+app.run(host="0.0.0.0",port=80)
+EOF
+
+python3 app.py
