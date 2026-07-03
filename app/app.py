4cdbe14fad15b5f989639624c4b17e7b00cf437a
DikshaPachpande <dikshapachpande@gmail.com>
2026-07-03 18:29:30 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template_string
+import socket
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    hostname = socket.gethostname()
+
+    return f"""
+    <html>
+    <body>
+        <h1>Scalable Web App</h1>
+        <h2>Server: {hostname}</h2>
+        <p>Running on AWS EC2</p>
+    </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
